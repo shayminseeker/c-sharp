85b575e24e2f40702b19e73de8a789a3533fe9ae
Lucas Norton <snowfalcon2020@gmail.com>
2026-01-06 09:56:22 -0700

    finish2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 int a = 1;
 int b = 0;
 Console.WriteLine("a: " + a + " b: " + b);
@@ -37,6 +39,39 @@ int calculation = 34 * 17 - 12/6;
 calculation -= (9*3);
 Console.WriteLine(calculation);
 
+//expression 1
 Console.WriteLine((4 * (7 - 9) + 11) / 3);
 int ex1 = (4 * (7 - 9) + 11) / 3;
 Console.WriteLine(ex1);
+//expression 2
+Console.WriteLine(3 == 2 && 4>1) || false ;
+int ex2 = (3 == 2 && 4>1) || false;
+Console.WriteLine(ex2);
+//expression 3
+Console.WriteLine(32.1-3.445) *17.7 /(2.38 +21.9);
+double ex3 = (32.1-3.445) *17.7 /(2.38 +21.9);
+Console.WriteLine(ex3);
+
+//expression 1.a
+Console.WriteLine(4 * 7 - 9 + 11 / 3);
+int ex1 = 4 * 7 - 9 + 11 / 3;
+Console.WriteLine(ex1);
+//expression 2.a
+Console.WriteLine(3 == 2 && 4>1 || false);
+int ex2 = 3 == 2 && 4>1 || false;
+Console.WriteLine(ex2);
+//expression 3.a
+Console.WriteLine(32.1-3.445 *17.7 /2.38 +21.9);
+double ex3 = 32.1-3.445 *17.7 /2.38 +21.9;
+Console.WriteLine(ex3);
+//Type-inffered
+int declared = 28;
+var inferred = declared * 2.13;
+bool isDeclared = true;
+var isInferred = 2 > 3;
+var alsoInferred = $"{inferred}";
+Console.WriteLine(declared.GetType());
+Console.WriteLine(inferred.GetType());
+Console.WriteLine(isDeclared.GetType());
+Console.WriteLine(isInferred.GetType());
+Console.WriteLine(alsoInferred.GetType());
